d4dcafe4cb7c15158663fae59d322ee9b638cba2
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-10-30 13:31:05 +0100

    Models + usuario

--- bookstore/storeapp/models.py
+++ bookstore/storeapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    isbn = models.CharField(max_length=13)
+    titulo = models.CharField(max_length=50)
+    autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
+    genero = models.ForeignKey('Genero', on_delete=models.CASCADE)
+
+class Autor(models.Model):
+    autorId = models.AutoField
+    nombre = models.CharField(max_length=50)
+
+class Genero(models.Model):
+    generoId = models.AutoField
+    genero = models.CharField(max_length=30)
+
+#métodos
