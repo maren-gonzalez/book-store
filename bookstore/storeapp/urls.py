25fcdc81c53ab72ecace375616f6950cf008d21b
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-07 13:39:44 +0100

    maping de las páginas

--- bookstore/storeapp/urls.py
+++ bookstore/storeapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.loginform, name='index'),
- # ej: /miApp/empresas/
- path('libro/', views.libro, name='libro'),
- path('autor/', views.autor, name='autor'),
- 
+ path('', views.index, name='index'),
+ path('products.html/', views.libros, name='productos'),
+ path('contact.html/', views.contacto, name='contacto'),
+ path('about.html/', views.login, name='login')
 ]
