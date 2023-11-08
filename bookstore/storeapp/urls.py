9667fff9079d744bbdf0d70dcb5b5d9da0033107
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-08 18:49:30 +0100

    Mappings de secciones

--- bookstore/storeapp/urls.py
+++ bookstore/storeapp/urls.py
@@ -3,9 +3,11 @@ from . import views
 urlpatterns = [
  path('', views.index, name='index'),
  path('gallery.html/', views.gallery, name='gallery'),
+ path('gallery.html/<str:categoria>', views.gallery, name='gallery'),
  path('contact.html/', views.contact, name='contact'),
  path('about.html/', views.about, name='about'),
  path('libros.html/', views.libros, name='libros'),
+ path('libros.html/<int:id_libro>', views.libros, name='libros'),
  path('autor.html/', views.autor, name='autor'),
-  path('genero.html/', views.genero, name='genero'),
+ path('genero.html/', views.genero, name='genero'),
 ]
