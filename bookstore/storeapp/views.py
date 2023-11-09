e7336c6413d271bc25f0731a31a296a64d398c7c
ainhoa-conde <113512130+ainhoa-conde@users.noreply.github.com>
2023-11-09 18:57:12 +0100

    views arreglo

--- bookstore/storeapp/views.py
+++ bookstore/storeapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Libro, Autor, Editorial, modeloGuardar
+from .models import Libro, Autor, Editorial
 from .forms import InicioSesion
 # Create your views here.
 
@@ -9,24 +9,6 @@ def loginform(request):
  return render(
  request, 'forms.html', {'form':form}
  )
-
-def get_datos(request):
- if request.method == 'POST':
- # Crear una instancia del formulario y asociar los datos recibidos:
-    form = InicioSesion(request.POST)
-    # Validar los datos:
-    if form.is_valid():
-        mail = form.cleaned_data['email']
-        cont = form.cleaned_data['contra']
-        guardar = modeloGuardar(email=mail, contra=cont)
-        guardar.save()
-        return HttpResponseRedirect('/index/')
- # Si el metodo es GET (o cualquier otro) crear un formulario vacío
- else:
-    form = InicioSesion()
- # Renderizar la plantilla con el formulario (con los datos anteriores o vacío)
- return render(request, 'forms.html', {'form': form})
-
 
 def index(request):
     return render(request, 'index.html')
