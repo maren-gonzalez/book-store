dfdb599ab328bf4cc3a42da01cda6c8d9663d323
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-09 14:44:12 +0100

    urls para los nuevos mappings

--- bookstore/storeapp/urls.py
+++ bookstore/storeapp/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
  path('contact.html/', views.contact, name='contact'),
  path('about.html/', views.about, name='about'),
  path('libros.html/', views.libros, name='libros'),
- path('libros.html/<int:id_libro>', views.libros, name='libros'),
+ path('libros.html/<str:isbn>', views.libros, name='libros'),
  path('autor.html/', views.autor, name='autor'),
+ path('autor.html/<str:id_autor>', views.autor, name='autor'),
  path('genero.html/', views.genero, name='genero'),
+ path('genero.html/<str:id_genero>', views.genero, name='genero')
 ]
