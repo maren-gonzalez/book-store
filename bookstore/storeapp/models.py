493be7b2106a197e03a31dcc842ecb392493c24a
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-09 19:42:41 +0100

    cambio de modelos

--- bookstore/storeapp/models.py
+++ bookstore/storeapp/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.forms import ModelForm
-from django import forms
 
 # Create your models here.
 class Libro(models.Model):
     isbn = models.CharField(max_length=13, primary_key=True)
     titulo = models.CharField(max_length=50)
-    autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
-    editorial = models.ForeignKey('Editorial', on_delete=models.CASCADE)
+    autor = models.ManyToManyField('Autor', on_delete=models.CASCADE, default = '')
+    editorial = models.ForeignKey('Editorial', on_delete=models.CASCADE, default = '')
 
 class Autor(models.Model):
-    autorId = models.IntegerField(primary_key=True)
+    autorId = models.IntegerField(primary_key=True, default=0)
     nombre = models.CharField(max_length=50)
-    fechanac = models.DateField()
+    fechanac = models.DateField(default='1999-01-01')
 
 class Editorial(models.Model):
-    editorialId = models.IntegerField(primary_key=True)
-    editorial = models.CharField(max_length=30)
+    editorialId = models.IntegerField(primary_key=True, default=0)
+    editorial = models.CharField(max_length=30, default = '')
