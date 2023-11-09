365e1cf54060beb2d3206b20f75c0a786c5595f2
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-09 19:03:13 +0100

    corregir views

--- bookstore/storeapp/views.py
+++ bookstore/storeapp/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Libro, Autor, Editorial
-from .forms import InicioSesion
+
 # Create your views here.
 
-def loginform(request):
- form = InicioSesion()
- return render(
- request, 'forms.html', {'form':form}
- )
+
 
 def index(request):
     return render(request, 'index.html')
