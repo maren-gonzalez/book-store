e7d97ecf9883652dcc5d4bff196e3fa7d27f3ede
maren <113512118+maren-gonzalez@users.noreply.github.com>
2023-11-26 16:53:46 +0100

    urls arreglados

--- bookstore/storeapp/urls.py
+++ bookstore/storeapp/urls.py
@@ -2,16 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path('', views.index, name='index'),
- path('gallery.html/', views.gallery, name='gallery'),
- path('gallery.html/<str:categoria>', views.gallery, name='gallery'),
- path('contact.html/', views.contact, name='contact'),
- path('about.html/', views.about, name='about'),
- path('libros.html/', views.libros, name='libros'),
+ path('gallery', views.gallery, name='gallery'),
+ path('contact', views.contact, name='contact'),
+ path('libros', views.libros, name='libros'),
  path('libro/<str:isbn>', views.libroDetalle, name='libroDetalle'),
- path('autor.html/', views.autor, name='autor'),
- path('autor.html/<int:autorId>', views.autor, name='autor'),
+ path('autor', views.autor, name='autor'),
  path('autor/<int:autorId>', views.autorDetalle, name='autorDetalle'),
- path('editorial.html/', views.editorial, name='editorial'),
- path('editorial.html/<str:editorialId>', views.editorial, name='editorial'),
+ path('editorial', views.editorial, name='editorial'),
  path('editorial/<int:editorialId>', views.editorialDetalle, name='editorialDetalle'),
 ]
